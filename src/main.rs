eeaf1976a2cfdad6a2dede533d20683219da9127
Martichou <martichou.andre@gmail.com>
2021-01-27 12:45:52 +0100

    Improved PoC thanks to updated rust_postgres WIP PR

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,77 @@
 #[macro_use]
 extern crate log;
 
+use futures::StreamExt;
 use postgres_protocol::message::backend::ReplicationMessage;
-use tokio::stream::StreamExt;
-use tokio_postgres::{
-    connect_replication, replication_client::SnapshotMode, types::Lsn, Error, NoTls,
-    ReplicationMode,
-};
-// use rand::Rng;
+use std::time::{Duration, UNIX_EPOCH};
+use tokio_postgres::replication_client::SnapshotMode;
+use tokio_postgres::{connect_replication, Error, NoTls, ReplicationMode};
 
 mod logger;
 
-// fn random_slot_name() -> String {
-//     let mut rng = rand::thread_rng();
-//     let array: [&str; 6] = [
-//         &"gigantic",
-//         &"scold",
-//         &"greasy",
-//         &"shaggy",
-//         &"wasteful",
-//         &"ablaze",
-//     ];
-
-//     let rnbr = rng.gen_range(0..6);
-//     format!("spc_{}{}", array[rnbr], rng.gen_range(10..100))
-// }
+const TIME_SEC_CONVERSION: u64 = 946_684_800;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // Init the logger and set the debug level correctly
     logger::configure();
 
+    // Connection information to postgres
     let conninfo = "host=localhost user=postgres dbname=pgcdc password=password";
 
-    // form replication connection
-    let (mut rc, rco) = connect_replication(conninfo, NoTls, ReplicationMode::Logical)
+    // Form replication connection
+    let (mut rclient, rconnection) = connect_replication(conninfo, NoTls, ReplicationMode::Logical)
         .await
         .unwrap();
 
-    // this connection will stay alive as long as we keep sending status update (keepalive)
+    // Spawn connection to run on its own
     tokio::spawn(async move {
-        if let Err(e) = rco.await {
+        if let Err(e) = rconnection.await {
             panic!("connection error: {}", e);
         }
     });
 
-    let identify_system = rc.identify_system().await?;
+    // Define constants for the logical slot
+    let slot_name = "pgcdc_repl";
+    let plugin = "wal2json";
+    let options = &[("pretty-print", "0")];
 
-    let slot = "fixed_name_for_now";
+    // Clean up previous runs
+    rclient.drop_replication_slot(slot_name, true).await?;
 
-    let _ = rc.drop_replication_slot(slot, false).await.unwrap();
-
-    let _ = rc
-        .create_logical_replication_slot(
-            slot,
-            true,
-            "wal2json",
-            Some(SnapshotMode::NoExportSnapshot),
-        )
+    // We set NoExportSnapshot
+    let no_export = Some(SnapshotMode::NoExportSnapshot);
+    rclient
+        .create_logical_replication_slot(slot_name, true, plugin, no_export)
         .await?;
 
-    let mut rcc = rc.clone();
-    let mut logical_stream = rcc
-        .start_logical_replication(
-            slot,
-            identify_system.xlogpos(),
-            &vec![("pretty-print", "0")],
-        )
+    let identify_system = rclient.identify_system().await?;
+
+    // We now switch to consuming the stream
+    let mut logical_stream = rclient
+        .start_logical_replication(slot_name, identify_system.xlogpos(), options)
         .await?;
+
+    let epoch = UNIX_EPOCH + Duration::from_secs(TIME_SEC_CONVERSION);
+    let mut last_lsn = identify_system.xlogpos();
 
     while let Some(replication_message) = logical_stream.next().await {
         match replication_message? {
             ReplicationMessage::XLogData(xlog_data) => {
                 let json = std::str::from_utf8(xlog_data.data()).unwrap();
-                info!("JSON  text: {}", json);
+                info!("Json: {}", json);
+
+                last_lsn = xlog_data.wal_end().into();
+                info!("Last_lsn == {}", last_lsn);
             }
             ReplicationMessage::PrimaryKeepAlive(keepalive) => {
                 if keepalive.reply() == 1 {
-                    warn!("Keepalive is equals to 1");
-                    let wal_end: Lsn = keepalive.wal_end().into();
-                    rc.standby_status_update(wal_end, wal_end, wal_end, keepalive.timestamp(), 0)
+                    warn!("sending keepalive reply with last_lsn == {}", last_lsn);
+                    let ts = epoch.elapsed().unwrap().as_micros() as i64;
+                    logical_stream
+                        .as_mut()
+                        .standby_status_update(last_lsn, last_lsn, last_lsn, ts, 0)
                         .await?;
-                    info!("Status update sent");
                 }
             }
             _ => (),
