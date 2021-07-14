c8a306c3d8bbc1d0e85398b4b45cd0f47ed4594e
Martichou <martichou.andre@gmail.com>
2021-07-14 14:48:39 +0200

    Switch to Rustls from Openssl for HTTPS

--- src/server.rs
+++ src/server.rs
@@ -2,22 +2,40 @@ use super::CONFIG;
 use super::{websockets::handlers, websockets::server::ws_server::WsServer, TABLES};
 
 use actix_web::{middleware, App, HttpServer};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use rustls::internal::pemfile::{certs, pkcs8_private_keys, rsa_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
+use std::fs::File;
+use std::io::BufReader;
 
-/// Return the SslAcceptorBuilder needed for Actix to be binded on HTTPS
+/// Return the ServerConfig needed for Actix to be binded on HTTPS
 ///
-/// Use KEY_PRIV and KEY_CERT environement variable for the path to find the files.
-fn get_ssl_builder() -> openssl::ssl::SslAcceptorBuilder {
-    // Getting the KEY path for both cert & priv key
-    let key = CONFIG.get_str("KEY_PRIV").expect("BINDING must be set");
-    let cert = CONFIG.get_str("KEY_CERT").expect("BINDING must be set");
-    // Construct the SslAcceptor builder by setting the SslMethod as tls.
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    // Add the files (key & cert) to the builder
-    builder.set_private_key_file(key, SslFiletype::PEM).unwrap();
-    builder.set_certificate_chain_file(cert).unwrap();
+/// Use key and cert for the path to find the files.
+pub fn get_ssl_builder(key: String, cert: String) -> ServerConfig {
+    // Init the ServerConfig with no Client's cert verifiction
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    // Open BufReader on the key and cert files to read their content
+    let cert_file = &mut BufReader::new(
+        File::open(&cert).unwrap_or_else(|_| panic!("Certificate file not found at {}", cert)),
+    );
+    let key_file = &mut BufReader::new(
+        File::open(&key).unwrap_or_else(|_| panic!("Key file not found at {}", key)),
+    );
+    // Create a Vec of certificate by extracting all cert from cert_file
+    let cert_chain = certs(cert_file).unwrap();
+    // Extract all PKCS8-encoded private key from key_file and generate a Vec from them
+    let mut keys = pkcs8_private_keys(key_file).unwrap();
+    // If no keys are found, we try using the rsa type
+    if keys.is_empty() {
+        // Reopen a new BufReader as pkcs8_private_keys took over the previous one
+        let key_file = &mut BufReader::new(
+            File::open(&key).unwrap_or_else(|_| panic!("Key file not found at {}", key)),
+        );
+        keys = rsa_private_keys(key_file).unwrap();
+    }
+    // Set a single certificate to be used for all subsequent request
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
 
-    builder
+    config
 }
 
 /// Construct and run the actix server instance.
@@ -48,6 +66,14 @@ pub async fn server(wsc: actix::Addr<WsServer>) -> std::io::Result<()> {
         serv.bind(binding)?.run().await
     } else {
         info!("Server started as HTTPS");
-        serv.bind_openssl(binding, get_ssl_builder())?.run().await
+        serv.bind_rustls(
+            binding,
+            get_ssl_builder(
+                CONFIG.get_str("KEY_PRIV").unwrap(),
+                CONFIG.get_str("KEY_CERT").unwrap(),
+            ),
+        )?
+        .run()
+        .await
     }
 }
