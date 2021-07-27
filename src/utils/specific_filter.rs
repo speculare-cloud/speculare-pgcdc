972ba73e34185935f86d79a7c6aaff92ebc65654
Martichou <martichou.andre@gmail.com>
2021-07-27 15:29:28 +0200

    Remove not needed tmp value

--- src/utils/specific_filter.rs
+++ src/utils/specific_filter.rs
@@ -14,14 +14,10 @@ pub struct SpecificFilter {
 impl SpecificFilter {
     /// Determine if the filter match the message passed as parameter
     pub fn match_filter(&self, message: &serde_json::Value) -> bool {
-        // Define easy to use variable
-        let column = &self.column;
-        let value = &self.value;
-
         // Determine if the column is present in this change
         let columns = message["columnnames"].as_array().unwrap();
         // Check if the cloumns we asked for exist in this data change
-        let value_index = columns.iter().position(|c| c == column);
+        let value_index = columns.iter().position(|c| c == &self.column);
         if value_index.is_none() {
             return false;
         }
@@ -29,7 +25,7 @@ impl SpecificFilter {
         if let Some(col_vals) = message["columnvalues"].as_array() {
             let targeted_value = &col_vals[value_index.unwrap()];
             // If the value we asked for is a String or a Number
-            return match value {
+            return match &self.value {
                 DataType::String(val) => {
                     // Check if is_string, and if so, convert it then check
                     match targeted_value.as_str() {
