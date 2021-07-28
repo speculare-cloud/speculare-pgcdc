4541c2711c7b2dd614c841727bf9b7a3f6ff6aa2
Martichou <martichou.andre@gmail.com>
2021-07-28 02:13:39 +0200

    Implement TLS if needed for Postgres connection & streaming

--- src/cdc/connection.rs
+++ src/cdc/connection.rs
@@ -1,11 +1,14 @@
 use crate::CONFIG;
 
+use openssl::ssl::{SslConnector, SslMethod};
+use postgres_openssl::MakeTlsConnector;
 use tokio_postgres::{Client, NoTls};
 
 /// Connects to the Postgres server (using conn_string for server info)
 pub async fn db_client_start() -> Client {
+    let tls = CONFIG.get_bool("POSTGRES_TLS").unwrap_or(true);
     let conn_string = format!(
-        "host={} user={} dbname={} replication=database password={}",
+        "host={} user={} dbname={} replication=database password={} sslmode={}",
         CONFIG
             .get_str("POSTGRES_HOST")
             .expect("Missing POSTGRES_HOST inside the config"),
@@ -17,24 +20,53 @@ pub async fn db_client_start() -> Client {
             .expect("Missing POSTGRES_DATABASE inside the config"),
         CONFIG
             .get_str("POSTGRES_PASSWORD")
-            .expect("Missing POSTGRES_PASSWORD inside the config")
+            .expect("Missing POSTGRES_PASSWORD inside the config"),
+        if tls { "require" } else { "allow" }
     );
 
-    let (client, connection) = match tokio_postgres::connect(&conn_string, NoTls).await {
-        Ok((rc, rco)) => (rc, rco),
-        Err(e) => {
-            error!("Postgres: connection failed: {}", e);
-            std::process::exit(1);
+    // TODO - Avoid all the duplicated code - Maybe using a macro ?
+    let client = match tls {
+        true => {
+            let connector =
+                MakeTlsConnector::new(SslConnector::builder(SslMethod::tls()).unwrap().build());
+            let (rc, rco) = match tokio_postgres::connect(&conn_string, connector).await {
+                Ok((rc, rco)) => (rc, rco),
+                Err(e) => {
+                    error!("Postgres: connection failed: {}", e);
+                    std::process::exit(1);
+                }
+            };
+
+            tokio::spawn(async move {
+                if let Err(e) = rco.await {
+                    error!("Postgres: connection broken due to: {}", e);
+                    std::process::exit(1);
+                }
+            });
+
+            rc
+        }
+        false => {
+            let connector = NoTls;
+            let (rc, rco) = match tokio_postgres::connect(&conn_string, connector).await {
+                Ok((rc, rco)) => (rc, rco),
+                Err(e) => {
+                    error!("Postgres: connection failed: {}", e);
+                    std::process::exit(1);
+                }
+            };
+
+            tokio::spawn(async move {
+                if let Err(e) = rco.await {
+                    error!("Postgres: connection broken due to: {}", e);
+                    std::process::exit(1);
+                }
+            });
+
+            rc
         }
     };
     trace!("Postgres: connection etablished");
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            error!("Postgres: connection broken due to: {}", e);
-            std::process::exit(1);
-        }
-    });
 
     client
 }
