8992eeb4bfbe5d1691ab2c718b44ff14e0d46e0a
Martichou <m@rtin.fyi>
2022-07-06 23:41:33 +0200

    remove unwrap after simple_query for tables fetching

--- src/cdc/mod.rs
+++ src/cdc/mod.rs
@@ -31,18 +31,19 @@ impl ExtConfig for Client {
         let query = "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE' AND table_name!='__diesel_schema_migrations';";
         TABLES.write().unwrap().clear();
 
-        self.simple_query(query)
-            .await
-            .unwrap()
-            .into_iter()
-            .for_each(|msg| {
+        match self.simple_query(query).await {
+            Ok(res) => res.into_iter().for_each(|msg| {
                 // And push them to the TABLES Vec
                 if let SimpleQueryMessage::Row(row) = msg {
                     if let Some(val) = row.get(0) {
                         TABLES.write().unwrap().push(val.to_owned())
                     }
                 }
-            });
+            }),
+            Err(err) => {
+                error!("Cannot check the tables, continuing without them: {}", err);
+            }
+        }
     }
 
     #[cfg(feature = "timescale")]
@@ -51,11 +52,8 @@ impl ExtConfig for Client {
             "select table_name,associated_table_prefix from _timescaledb_catalog.hypertable;";
         TABLES_LOOKUP.write().unwrap().clear();
 
-        self.simple_query(query)
-            .await
-            .unwrap()
-            .into_iter()
-            .for_each(|msg| {
+        match self.simple_query(query).await {
+            Ok(res) => res.into_iter().for_each(|msg| {
                 if let SimpleQueryMessage::Row(row) = msg {
                     if let (Some(table), Some(prefix)) = (row.get(0), row.get(1)) {
                         if let Ok(idx) = extract_hyper_idx(prefix) {
@@ -63,6 +61,13 @@ impl ExtConfig for Client {
                         }
                     }
                 }
-            });
+            }),
+            Err(err) => {
+                error!(
+                    "Cannot check the lookup tables, continuing without them: {}",
+                    err
+                );
+            }
+        }
     }
 }
